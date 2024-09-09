c2c67cc15219a60a07e884c8eed99fe622b5a2f0
Victor Mezrin <mezrinv@gmail.com>
2024-09-09 09:04:47 -0700

    add tests

--- py-tests/test_consistency.py
+++ py-tests/test_consistency.py
@@ -1,7 +1,9 @@
+import http
 import json
 import unittest
 from pathlib import Path
 
+import requests
 from traitsvalidator import TraitsValidator
 
 
@@ -9,6 +11,7 @@ class TestConfig:
     GIT_REPO_ROOT: Path = Path(__file__).parent.parent
     AWS_S3_ASSETS_DIR: Path = GIT_REPO_ROOT / "aws_s3_assets"
     AWS_S3_URL = f"https://trait-wallet-demo-account.s3.us-east-1.amazonaws.com"
+    HTTP_TIMEOUT = 2
 
 
 class TestRegistry(unittest.TestCase):
@@ -64,12 +67,14 @@ class TestRegistry(unittest.TestCase):
         for meta_path in meta_app_agents:
             with meta_path.open() as fp:
                 appagent_meta = json.load(fp)
+
             available_traits = validator.validate_metadata(appagent_meta)
             self.assertEqual(
                 available_traits,
                 ["named", "tech.trait.wallet.square_icon"],
                 f"Unexpected set of traits in the file {meta_path}",
             )
+
             icon_url: str = appagent_meta["traits"]["tech.trait.wallet.square_icon"]["image_url"]
             icon_path = icon_url.replace(TestConfig.AWS_S3_URL, str(TestConfig.AWS_S3_ASSETS_DIR))
             self.assertTrue(Path(icon_path).is_file())
@@ -80,12 +85,14 @@ class TestRegistry(unittest.TestCase):
         for meta_path in meta_fungibles:
             with meta_path.open() as fp:
                 fungible_meta = json.load(fp)
+
             available_traits = validator.validate_metadata(fungible_meta)
             self.assertEqual(
                 available_traits,
                 ["named", "fungible", "tech.trait.wallet.square_icon"],
                 f"Unexpected set of traits in the file {meta_path}",
             )
+
             icon_url: str = fungible_meta["traits"]["tech.trait.wallet.square_icon"]["image_url"]
             icon_path = icon_url.replace(TestConfig.AWS_S3_URL, str(TestConfig.AWS_S3_ASSETS_DIR))
             self.assertTrue(Path(icon_path).is_file())
@@ -96,6 +103,7 @@ class TestRegistry(unittest.TestCase):
             with meta_path.open() as fp:
                 nft_collection_meta = json.load(fp)
             available_traits = validator.validate_metadata(nft_collection_meta)
+
             self.assertEqual(
                 available_traits,
                 [
@@ -105,6 +113,7 @@ class TestRegistry(unittest.TestCase):
                 ],
                 f"Unexpected set of traits in the file {meta_path}",
             )
+
             icon_url: str = nft_collection_meta["traits"]["tech.trait.wallet.square_icon"]["image_url"]
             icon_path = icon_url.replace(TestConfig.AWS_S3_URL, str(TestConfig.AWS_S3_ASSETS_DIR))
             self.assertTrue(Path(icon_path).is_file())
@@ -119,6 +128,7 @@ class TestRegistry(unittest.TestCase):
             with meta_path.open() as fp:
                 nft_token_meta = json.load(fp)
             available_traits = validator.validate_metadata(nft_token_meta)
+
             self.assertEqual(
                 available_traits,
                 [
@@ -131,6 +141,7 @@ class TestRegistry(unittest.TestCase):
                 ],
                 f"Unexpected set of traits in the file {meta_path}",
             )
+
             icon_url: str = nft_token_meta["traits"]["tech.trait.wallet.square_icon"]["image_url"]
             icon_path = icon_url.replace(TestConfig.AWS_S3_URL, str(TestConfig.AWS_S3_ASSETS_DIR))
             self.assertTrue(Path(icon_path).is_file())
@@ -143,3 +154,127 @@ class TestRegistry(unittest.TestCase):
             cover_path = cover_url.replace(TestConfig.AWS_S3_URL, str(TestConfig.AWS_S3_ASSETS_DIR))
             self.assertTrue(Path(cover_path).is_file())
             self.assertTrue(Path(cover_path).name.endswith(".png"))
+
+    def test_validate_s3_meta(self: "TestRegistry") -> None:
+        """
+        Validate metadata of on-chain assets stored in the AWS S3 bucket.
+
+        This function iterates over all metadata files in the AWS S3 bucket, validates their traits.
+        It also checks existince of the images refered in metadata files.
+        """
+        validator = TraitsValidator()
+
+        # Collect list of all metadata files
+        all_meta = self.collect_all_meta()
+        meta_app_agents = all_meta[0]
+        meta_fungibles = all_meta[1]
+        meta_nft_collections = all_meta[2]
+        meta_nft_tokens = all_meta[3]
+
+        # Validate AppAgent metadata
+        for meta_path in meta_app_agents:
+            meta_url = str(meta_path).replace(str(TestConfig.AWS_S3_ASSETS_DIR), TestConfig.AWS_S3_URL)
+            resp = requests.get(url=meta_url, timeout=TestConfig.HTTP_TIMEOUT)
+            if resp.status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the metadata file in the S3 bucket: `{meta_url}`."
+                raise ValueError(msg)
+            appagent_meta = resp.json()
+
+            available_traits = validator.validate_metadata(appagent_meta)
+            self.assertEqual(
+                available_traits,
+                ["named", "tech.trait.wallet.square_icon"],
+                f"Unexpected set of traits in the file {meta_path}",
+            )
+
+            icon_url: str = appagent_meta["traits"]["tech.trait.wallet.square_icon"]["image_url"]
+            if requests.get(url=icon_url, timeout=TestConfig.HTTP_TIMEOUT).status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the image in the S3 bucket: `{icon_url}`."
+                raise ValueError(msg)
+
+
+        # Validate metadata of fungible token
+        for meta_path in meta_fungibles:
+            meta_url = str(meta_path).replace(str(TestConfig.AWS_S3_ASSETS_DIR), TestConfig.AWS_S3_URL)
+            resp = requests.get(url=meta_url, timeout=TestConfig.HTTP_TIMEOUT)
+            if resp.status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the metadata file in the S3 bucket: `{meta_url}`."
+                raise ValueError(msg)
+            fungible_meta = resp.json()
+
+            available_traits = validator.validate_metadata(fungible_meta)
+            self.assertEqual(
+                available_traits,
+                ["named", "fungible", "tech.trait.wallet.square_icon"],
+                f"Unexpected set of traits in the file {meta_path}",
+            )
+
+            icon_url: str = fungible_meta["traits"]["tech.trait.wallet.square_icon"]["image_url"]
+            if requests.get(url=icon_url, timeout=TestConfig.HTTP_TIMEOUT).status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the image in the S3 bucket: `{icon_url}`."
+                raise ValueError(msg)
+
+        # Validate metadata of nft collection
+        for meta_path in meta_nft_collections:
+            meta_url = str(meta_path).replace(str(TestConfig.AWS_S3_ASSETS_DIR), TestConfig.AWS_S3_URL)
+            resp = requests.get(url=meta_url, timeout=TestConfig.HTTP_TIMEOUT)
+            if resp.status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the metadata file in the S3 bucket: `{meta_url}`."
+                raise ValueError(msg)
+            nft_collection_meta = resp.json()
+
+            available_traits = validator.validate_metadata(nft_collection_meta)
+            self.assertEqual(
+                available_traits,
+                [
+                    "named",
+                    "tech.trait.wallet.square_icon",
+                    "tech.trait.wallet.nft_collection_listing_image",
+                ],
+                f"Unexpected set of traits in the file {meta_path}",
+            )
+
+            icon_url: str = nft_collection_meta["traits"]["tech.trait.wallet.square_icon"]["image_url"]
+            if requests.get(url=icon_url, timeout=TestConfig.HTTP_TIMEOUT).status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the image in the S3 bucket: `{icon_url}`."
+                raise ValueError(msg)
+            listing_url: str = nft_collection_meta["traits"]["tech.trait.wallet.nft_collection_listing_image"]["image_url"]
+            if requests.get(url=listing_url, timeout=TestConfig.HTTP_TIMEOUT).status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the image in the S3 bucket: `{listing_url}`."
+                raise ValueError(msg)
+
+        # Validate metadata of nft token
+        for meta_path in meta_nft_tokens:
+            meta_url = str(meta_path).replace(str(TestConfig.AWS_S3_ASSETS_DIR), TestConfig.AWS_S3_URL)
+            resp = requests.get(url=meta_url, timeout=TestConfig.HTTP_TIMEOUT)
+            if resp.status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the metadata file in the S3 bucket: `{meta_url}`."
+                raise ValueError(msg)
+            nft_token_meta = resp.json()
+
+            available_traits = validator.validate_metadata(nft_token_meta)
+            self.assertEqual(
+                available_traits,
+                [
+                    "named",
+                    "tech.trait.wallet.square_icon",
+                    "tech.trait.wallet.nft_token_listing_image",
+                    "tech.trait.wallet.nft_token_cover_image",
+                    "tech.trait.wallet.nft_token_description",
+                    "tech.trait.wallet.nft_token_attributes",
+                ],
+                f"Unexpected set of traits in the file {meta_path}",
+            )
+
+            icon_url: str = nft_token_meta["traits"]["tech.trait.wallet.square_icon"]["image_url"]
+            if requests.get(url=icon_url, timeout=TestConfig.HTTP_TIMEOUT).status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the image in the S3 bucket: `{icon_url}`."
+                raise ValueError(msg)
+            listing_url: str = nft_token_meta["traits"]["tech.trait.wallet.nft_token_listing_image"]["image_url"]
+            if requests.get(url=listing_url, timeout=TestConfig.HTTP_TIMEOUT).status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the image in the S3 bucket: `{listing_url}`."
+                raise ValueError(msg)
+            cover_url: str = nft_token_meta["traits"]["tech.trait.wallet.nft_token_cover_image"]["image_url"]
+            if requests.get(url=cover_url, timeout=TestConfig.HTTP_TIMEOUT).status_code != http.HTTPStatus.OK:
+                msg = f"Couldn't find the image in the S3 bucket: `{cover_url}`."
+                raise ValueError(msg)
